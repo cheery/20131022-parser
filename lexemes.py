9d5dd7a5b3fb99de365fd074db31408242e653fd
Henri Tuhola <cheery@boxbase.org>
2013-10-22 00:43:47 +0300

    lexemes for S-exprs

---
+++ lexemes.py
@@ -0,0 +1,17 @@
+class BeginList(object):
+    pass
+
+class EndList(object):
+    pass
+
+class Variable(object):
+    def __init__(self, string):
+        self.string = string
+
+class String(object):
+    def __init__(self, string):
+        self.string = string
+
+class Integer(object):
+    def __init__(self, string):
+        self.string = string
